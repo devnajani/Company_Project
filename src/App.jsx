6b8e92c1c9b313495047379410015fd913950d5f
devnajani <159873827+devnajani@users.noreply.github.com>
2024-12-13 12:24:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-// import Navbar from "./components/Navbar"
-// import { BrowserRouter,Routes,Route } from "react-router-dom"
-// import Bill from "./components/Bill"
-import { app } from "firebase/auth";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-const signupUser=()=>{
-  createUserWithEmailAndPassword(auth, "devnajani@gmail.com","devna123")  
-  .then((userCredential) => {
-
-
-})}
-const auth = getAuth(app);
+import Navbar from "./components/Navbar"
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Bill from "./components/Bill"
 
 function App() {
 
   return (
     <>
-    {/* <Navbar/>
+    <Navbar/>
       <BrowserRouter>
       <Routes>
         <Route path="/"element={<Navbar/>}></Route>
         <Route path="/bill"element={<Bill/>}></Route>
         </Routes>      
       </BrowserRouter>
-    </> */}
-    <button onClick={signupUser}>create user</button>
     </>
+  
   )
 }
 
