0c5066097ca423d1a771d11d2f9e229215414e9e
devnajani <159873827+devnajani@users.noreply.github.com>
2024-12-13 12:25:16 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,11 +1,1 @@
-import {initializeApp} from "firebase/app"
-const firebaseConfig = {
-    apiKey: "AIzaSyBUA0QcdTjBq60fkUVfsx_j1IGJqDCL9sw",
-    authDomain: "first-firebase-72bb2.firebaseapp.com",
-    projectId: "first-firebase-72bb2",
-    storageBucket: "first-firebase-72bb2.firebasestorage.app",
-    messagingSenderId: "128956764598",
-    appId: "1:128956764598:web:65be4e6400a43ecdfef117",
-    databaseURL:"https://first-firebase-72bb2-default-rtdb.firebaseio.com"
-  };
-export const app=initializeApp(firebaseConfig)  
+
